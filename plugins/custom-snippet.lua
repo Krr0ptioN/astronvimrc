288067584055892e0846161f65cd708dc9e615fd
Krr0ptioN <mardin-pop@protonmail.com>
2023-09-29 12:18:56 +0300

    feat(plugin): able to add custom snips for a target language

--- plugins/custom-snippet.lua
+++ plugins/custom-snippet.lua
@@ -1,22 +1,24 @@
 return {
-    "L3MON4D3/LuaSnip",
-    config = function(plugin, opts)
-        -- include the default astronvim config that calls the setup call
-        local types = require "luasnip.util.types"
-        require'luasnip'.config.setup({
-            ext_opts = {
-                [types.choiceNode] = {
-                    active = {virt_text = {{"●", "GruvboxOrange"}}}
-                },
-                [types.insertNode] = {
-                    active = {virt_text = {{"●", "GruvboxBlue"}}}
+    {
+        "L3MON4D3/LuaSnip",
+        config = function(plugin, opts)
+            -- include the default astronvim config that calls the setup call
+            local types = require "luasnip.util.types"
+            require'luasnip'.config.setup({
+                ext_opts = {
+                    [types.choiceNode] = {
+                        active = {virt_text = {{"●", "GruvboxOrange"}}}
+                    },
+                    [types.insertNode] = {
+                        active = {virt_text = {{"●", "GruvboxBlue"}}}
+                    }
                 }
+            })
+            require "plugins.configs.luasnip"(plugin, opts)
+            require("luasnip.loaders.from_vscode").lazy_load {
+                -- Adjust the path relative to your configuration directory
+                -- paths = {"./lua/user/snippets"}
             }
-        })
-        require "plugins.configs.luasnip"(plugin, opts)
-        require("luasnip.loaders.from_vscode").lazy_load {
-            -- Adjust the path relative to your configuration directory
-            paths = {"./lua/user/snippets"}
-        }
-    end
+        end
+    }
 }
