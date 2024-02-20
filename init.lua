ca2985a14302ba3cf8c52576fcb40295bdd25c1a
Krr0ptioN <mardin-pop@protonmail.com>
2024-02-20 14:54:57 +0300

    feat: telescope highlights and nvim cmp

--- init.lua
+++ init.lua
@@ -18,38 +18,87 @@ return {
     },
 
     -- Set colorscheme to use
-    colorscheme = "astrodark",
+    -- colorscheme = "astrodark",
+    highlights = {
+        -- set highlights for all themes
+        -- use a function override to let us use lua to retrieve colors from highlight group
+        -- there is no default table so we don't need to put a parameter for this function
+        init = function()
+            local get_hlgroup = require("astronvim.utils").get_hlgroup
+            local normal = get_hlgroup "Normal"
+            local fg, bg = normal.fg, normal.bg
+            local bg_alt = get_hlgroup("Visual").bg
+            local green = get_hlgroup("String").fg
+            local red = get_hlgroup("Error").fg
+
+            return {
+                TelescopeBorder = {fg = bg_alt, bg = bg},
+                TelescopeNormal = {bg = bg},
+                TelescopePreviewBorder = {fg = bg, bg = bg},
+                TelescopePreviewNormal = {bg = bg},
+                TelescopePreviewTitle = {fg = bg, bg = green},
+                TelescopePromptBorder = {fg = bg_alt, bg = bg_alt},
+                TelescopePromptNormal = {fg = fg, bg = bg_alt},
+                TelescopePromptPrefix = {fg = red, bg = bg_alt},
+                TelescopePromptTitle = {fg = bg, bg = red},
+                TelescopeResultsBorder = {fg = bg, bg = bg},
+                TelescopeResultsNormal = {bg = bg},
+                TelescopeResultsTitle = {fg = bg, bg = bg},
+
+                -- Nvim Cmp
+                PmenuSel = {bg = "#282C34", fg = "NONE"},
+                Pmenu = {fg = "#C5CDD9", bg = "#22252A"},
+                CmpItemAbbrDeprecated = {
+                    fg = "#7E8294",
+                    bg = "NONE",
+                    strikethrough = true
+                },
+                CmpItemAbbrMatch = {fg = "#82AAFF", bg = "NONE", bold = true},
+                CmpItemAbbrMatchFuzzy = {
+                    fg = "#82AAFF",
+                    bg = "NONE",
+                    bold = true
+                },
+                CmpItemMenu = {fg = "#C792EA", bg = "NONE", italic = true},
+
+                CmpItemKindField = {fg = "#EED8DA", bg = "#B5585F"},
+                CmpItemKindProperty = {fg = "#EED8DA", bg = "#B5585F"},
+                CmpItemKindEvent = {fg = "#EED8DA", bg = "#B5585F"},
+
+                CmpItemKindText = {fg = "#C3E88D", bg = "#9FBD73"},
+                CmpItemKindEnum = {fg = "#C3E88D", bg = "#9FBD73"},
+                CmpItemKindKeyword = {fg = "#C3E88D", bg = "#9FBD73"},
+
+                CmpItemKindConstant = {fg = "#FFE082", bg = "#D4BB6C"},
+                CmpItemKindConstructor = {fg = "#FFE082", bg = "#D4BB6C"},
+                CmpItemKindReference = {fg = "#FFE082", bg = "#D4BB6C"},
+
+                CmpItemKindFunction = {fg = "#EADFF0", bg = "#A377BF"},
+                CmpItemKindStruct = {fg = "#EADFF0", bg = "#A377BF"},
+                CmpItemKindClass = {fg = "#EADFF0", bg = "#A377BF"},
+                CmpItemKindModule = {fg = "#EADFF0", bg = "#A377BF"},
+                CmpItemKindOperator = {fg = "#EADFF0", bg = "#A377BF"},
+
+                CmpItemKindVariable = {fg = "#C5CDD9", bg = "#7E8294"},
+                CmpItemKindFile = {fg = "#C5CDD9", bg = "#7E8294"},
+
+                CmpItemKindUnit = {fg = "#F5EBD9", bg = "#D4A959"},
+                CmpItemKindSnippet = {fg = "#F5EBD9", bg = "#D4A959"},
+                CmpItemKindFolder = {fg = "#F5EBD9", bg = "#D4A959"},
+
+                CmpItemKindMethod = {fg = "#DDE5F5", bg = "#6C8ED4"},
+                CmpItemKindValue = {fg = "#DDE5F5", bg = "#6C8ED4"},
+                CmpItemKindEnumMember = {fg = "#DDE5F5", bg = "#6C8ED4"},
+
+                CmpItemKindInterface = {fg = "#D8EEEB", bg = "#58B5A8"},
+                CmpItemKindColor = {fg = "#D8EEEB", bg = "#58B5A8"},
+                CmpItemKindTypeParameter = {fg = "#D8EEEB", bg = "#58B5A8"}
+            }
+        end
+    },
 
     -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
     diagnostics = {virtual_text = true, underline = true},
-
-    lsp = {
-        -- customize lsp formatting options
-        formatting = {
-            -- control auto formatting on save
-            format_on_save = {
-                enabled = true, -- enable or disable format on save globally
-                allow_filetypes = { -- enable format on save for specified filetypes only
-                    -- "go",
-                },
-                ignore_filetypes = { -- disable format on save for specified filetypes
-                    -- "python",
-                }
-            },
-            disabled = { -- disable formatting capabilities for the listed language servers
-                -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
-                -- "lua_ls",
-            },
-            timeout_ms = 1000 -- default format timeout
-            -- filter = function(client) -- fully override the default formatting function
-            --   return true
-            -- end
-        },
-        -- enable servers that you already have installed without mason
-        servers = {
-            -- "pyright"
-        }
-    },
 
     -- Configure require("lazy").setup() options
     lazy = {
@@ -64,23 +113,5 @@ return {
             }
         }
     },
-
-    -- This function is run last and is a good place to configuring
-    -- augroups/autocommands and custom filetypes also this just pure lua so
-    -- anything that doesn't fit in the normal config locations above can go here
-    polish = function()
-        -- Set up custom filetypes
-        -- vim.filetype.add {
-        --   extension = {
-        --     foo = "fooscript",
-        --   },
-        --   filename = {
-        --     ["Foofile"] = "fooscript",
-        --   },
-        --   pattern = {
-        --     ["~/%.config/foo/.*"] = "fooscript",
-        --   },
-        -- }
-        require("todo-comments").setup()
-    end
+    polish = require('user.polish')
 }
