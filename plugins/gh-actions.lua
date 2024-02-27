d62289d2b003ef3698cc63566a07aff9f4fa8d4e
Krr0ptioN <mardin-pop@protonmail.com>
2024-02-27 12:08:31 +0300

    fix: github action plugin

--- plugins/gh-actions.lua
+++ plugins/gh-actions.lua
@@ -1,65 +1,3 @@
-config = {
-    --- The browser executable path to open workflow runs/jobs in
-    ---@type string|nil
-    browser = nil,
-    --- Interval to refresh in seconds
-    refresh_interval = 10,
-    --- How much workflow runs and jobs should be indented
-    indent = 2,
-    icons = {
-        workflow_dispatch = '⚡️',
-        conclusion = {
-            success = '✓',
-            failure = 'X',
-            startup_failure = 'X',
-            cancelled = '⊘',
-            skipped = '◌'
-        },
-        status = {
-            unknown = '?',
-            pending = '○',
-            queued = '○',
-            requested = '○',
-            waiting = '○',
-            in_progress = '●'
-        }
-    },
-    highlights = {
-        GhActionsRunIconSuccess = {link = 'LspDiagnosticsVirtualTextHint'},
-        GhActionsRunIconFailure = {link = 'LspDiagnosticsVirtualTextError'},
-        GhActionsRunIconStartup_failure = {
-            link = 'LspDiagnosticsVirtualTextError'
-        },
-        GhActionsRunIconPending = {link = 'LspDiagnosticsVirtualTextWarning'},
-        GhActionsRunIconRequested = {link = 'LspDiagnosticsVirtualTextWarning'},
-        GhActionsRunIconWaiting = {link = 'LspDiagnosticsVirtualTextWarning'},
-        GhActionsRunIconIn_progress = {
-            link = 'LspDiagnosticsVirtualTextWarning'
-        },
-        GhActionsRunIconCancelled = {link = 'Comment'},
-        GhActionsRunIconSkipped = {link = 'Comment'},
-        GhActionsRunCancelled = {link = 'Comment'},
-        GhActionsRunSkipped = {link = 'Comment'},
-        GhActionsJobCancelled = {link = 'Comment'},
-        GhActionsJobSkipped = {link = 'Comment'},
-        GhActionsStepCancelled = {link = 'Comment'},
-        GhActionsStepSkipped = {link = 'Comment'}
-    },
-    split = {
-        relative = 'editor',
-        position = 'right',
-        size = 60,
-        win_options = {
-            wrap = false,
-            number = false,
-            foldlevel = nil,
-            foldcolumn = '0',
-            cursorcolumn = false,
-            signcolumn = 'no'
-        }
-    }
-}
-
 return {
     'topaxi/gh-actions.nvim',
     cmd = 'GhActions',
@@ -67,6 +5,72 @@ return {
     -- optional, you can also install and use `yq` instead.
     build = 'make',
     dependencies = {'nvim-lua/plenary.nvim', 'MunifTanjim/nui.nvim'},
-    opts = {config},
+    opts = {
+        --- The browser executable path to open workflow runs/jobs in
+        ---@type string|nil
+        browser = nil,
+        --- Interval to refresh in seconds
+        refresh_interval = 2,
+        --- How much workflow runs and jobs should be indented
+        indent = 2,
+        icons = {
+            workflow_dispatch = '⚡️',
+            conclusion = {
+                success = '✓',
+                failure = 'X',
+                startup_failure = 'X',
+                cancelled = '⊘',
+                skipped = '◌'
+            },
+            status = {
+                unknown = '?',
+                pending = '○',
+                queued = '○',
+                requested = '○',
+                waiting = '○',
+                in_progress = '●'
+            }
+        },
+        highlights = {
+            GhActionsRunIconSuccess = {link = 'LspDiagnosticsVirtualTextHint'},
+            GhActionsRunIconFailure = {link = 'LspDiagnosticsVirtualTextError'},
+            GhActionsRunIconStartup_failure = {
+                link = 'LspDiagnosticsVirtualTextError'
+            },
+            GhActionsRunIconPending = {
+                link = 'LspDiagnosticsVirtualTextWarning'
+            },
+            GhActionsRunIconRequested = {
+                link = 'LspDiagnosticsVirtualTextWarning'
+            },
+            GhActionsRunIconWaiting = {
+                link = 'LspDiagnosticsVirtualTextWarning'
+            },
+            GhActionsRunIconIn_progress = {
+                link = 'LspDiagnosticsVirtualTextWarning'
+            },
+            GhActionsRunIconCancelled = {link = 'Comment'},
+            GhActionsRunIconSkipped = {link = 'Comment'},
+            GhActionsRunCancelled = {link = 'Comment'},
+            GhActionsRunSkipped = {link = 'Comment'},
+            GhActionsJobCancelled = {link = 'Comment'},
+            GhActionsJobSkipped = {link = 'Comment'},
+            GhActionsStepCancelled = {link = 'Comment'},
+            GhActionsStepSkipped = {link = 'Comment'}
+        },
+        split = {
+            relative = 'editor',
+            position = 'right',
+            size = 60,
+            win_options = {
+                wrap = false,
+                number = false,
+                foldlevel = nil,
+                foldcolumn = '0',
+                cursorcolumn = false,
+                signcolumn = 'no'
+            }
+        }
+    },
     config = function(_, opts) require('gh-actions').setup(opts) end
 }
